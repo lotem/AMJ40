b9bd2b1d3cf156ce147f5020b53b4703d1ca21de
Chen Gong <chen.sst@gmail.com>
2016-11-02 01:48:37 +0800

    feat(layout): layout switch keys

--- src/keymap_default.c
+++ src/keymap_default.c
@@ -5,19 +5,24 @@
 #define XXXXXXX KC_NO
 
 // function keys
-#define TO_1    KC_FN0  // toggle layer 1
-#define MO_2    KC_FN1  // momentary layer 2
-#define MO_3    KC_FN2  // momentary layer 3
-#define MO_4    KC_FN3  // momentary layer 4
-//#define L_2_SPC KC_FN4  // momentary layer 2 tap space
-//#define L_3_ENT KC_FN5  // momentary layer 3 tap enter
+#define RESET   KC_FN0
 
-#define BL_DEC  KC_FN6  // decrease backlight
-#define BL_INC  KC_FN7  // increase backlight
-#define BL_TOGG KC_FN8  // toggle backlight
-#define BL_STEP KC_FN9
+#define TO_1    KC_FN1  // toggle layer 1
+#define TO_2    KC_FN2
+#define TO_3    KC_FN3
+#define TO_4    KC_FN4
 
-#define RESET   KC_FN10
+#define L_7_SPC KC_FN5  // momentary layer 7 tap space
+#define L_8_ENT KC_FN6  // momentary layer 8 tap enter
+
+#define MO_7    KC_FN7  // momentary layer symbols
+#define MO_8    KC_FN8  // momentary layer shift symbols
+#define MO_9    KC_FN9  // momentary layer functions
+
+//#define BL_DEC  KC_FN6  // decrease backlight
+//#define BL_INC  KC_FN7  // increase backlight
+//#define BL_TOGG KC_FN8  // toggle backlight
+//#define BL_STEP KC_FN9
 
 #define KC_COLN KC_FN11 // :
 #define KC_DBQT KC_FN12 // "
@@ -54,39 +59,43 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * ,-----------------------------------------------------------.
      * | Tab|  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P | BS |
      * |-----------------------------------------------------------|
-     * | fn2  |  A |  S |  D |  F |  G |  H |  J |  K |  L | fn2   |
+     * | fna  |  A |  S |  D |  F |  G |  H |  J |  K |  L | fna   |
      * |-----------------------------------------------------------|
-     * | LSft   |  Z |  X |  C |  V |  B |  N |  M | /? | RSft| fn3|
+     * | LSft   |  Z |  X |  C |  V |  B |  N |  M | /? | RSft| fnb|
      * |-----------------------------------------------------------|
-     * | fn4 | Alt| Ctrl|   spc     |  ent        | Gui | Alt| fn4 |
+     * | Ctrl| Alt| Gui |   spc     |  ent        | Gui | Alt| fnc |
      * `-----------------------------------------------------------'
      */
     [0] = KEYMAP_LAYOUT_1(
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P,    KC_BSPC, \
-        MO_2   , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   ,          MO_2   , \
-        KC_LSFT,          KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_SLSH, KC_RSFT, MO_3   , \
-        MO_4   , KC_LALT, KC_LCTL, KC_SPC,                    KC_ENT ,                   KC_RGUI, KC_RALT, MO_4   ),
+        MO_7   , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   ,          MO_7   , \
+        KC_LSFT,          KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_SLSH, KC_RSFT, MO_8   , \
+        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,                    KC_ENT ,                   KC_RGUI, KC_RALT, MO_9   ),
 
-    /* Alternative Layer 1 Thumbs
-     * ,-----------------------------------------------------------.
-     * | Tab|  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P | BS |
-     * |-----------------------------------------------------------|
-     * | Ctrl |  A |  S |  D |  F |  G |  H |  J |  K |  L |  ;    |
-     * |-----------------------------------------------------------|
-     * | LSft   |  Z |  X |  C |  V |  B |  N |  M | /? | RSft| fn3|
-     * |-----------------------------------------------------------|
-     * | ALt | Gui| fn2 |  spc      |   ent       | fn2 | Gui| fn4 |
-     * `-----------------------------------------------------------'
-     */
+    // Layer 1 HHKB
     [1] = KEYMAP_LAYOUT_1(
-        KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P,    KC_BSPC, \
-        KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   ,          KC_SCLN, \
-        KC_LSFT,          KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_SLSH, KC_RSFT, MO_3   , \
-        KC_LALT, KC_LGUI, MO_2   , KC_SPC ,                   KC_ENT ,                   MO_2,    KC_RGUI, MO_4   ),
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        MO_7   , _______, _______,          _______,                   _______,          _______, _______, _______),
 
-    /* Symbols Layer 2
+    // Layer 2 thumb fn keys
+    [2] = KEYMAP_LAYOUT_1(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______,          L_7_SPC,                   L_8_ENT,          _______, _______, _______),
+
+    // Layer 3 enter
+    [3] = KEYMAP_LAYOUT_1(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_ENT , \
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______,          _______,                   L_7_SPC,          _______, _______, _______),
+
+    /* Symbols Layer
      * ,-----------------------------------------------------------.
-     * | Esc|  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |    |
+     * | Esc|  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 | Del|
      * |-----------------------------------------------------------|
      * |      |  \ |  [ |  ] |  - |  = |  ← |  ↓ |  ↑ |  → |       |
      * |-----------------------------------------------------------|
@@ -95,15 +104,15 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |     |    |     |           |             |     |    |     |
      * `-----------------------------------------------------------'
      */
-    [2] = KEYMAP_LAYOUT_1(
-        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______, \
+    [7] = KEYMAP_LAYOUT_1(
+        KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_DEL , \
         _______, KC_BSLS, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT,          _______, \
         _______,          KC_GRV , KC_LPRN, KC_RPRN, KC_SCLN, KC_QUOT, KC_COMM, KC_DOT , KC_SLSH, _______, _______, \
         _______, _______, _______,          _______,                   _______,          _______, _______, _______),
 
-    /* Symbols Layer 3 Shifted
+    /* Symbols Layer Shifted
      * ,-----------------------------------------------------------.
-     * | Esc|  ! |  @ |  # |  $ |  % |  ^ |  & |  * |  ( |  ) |    |
+     * | Esc|  ! |  @ |  # |  $ |  % |  ^ |  & |  * |  ( |  ) | Del|
      * |-----------------------------------------------------------|
      * |      |  | |  { |  } |  _ |  + |Home|PgDn|PgUp| End|       |
      * |-----------------------------------------------------------|
@@ -112,37 +121,28 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |     |    |     |           |             |     |    |     |
      * `-----------------------------------------------------------'
      */
-    [3] = KEYMAP_LAYOUT_1(
-        KC_ESC , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+    [8] = KEYMAP_LAYOUT_1(
+        KC_ESC , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL , \
         _______, KC_PIPE, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_HOME, KC_PGDN, KC_PGUP, KC_END ,          _______, \
         _______,          KC_TILD, KC_LPRN, KC_RPRN, KC_COLN, KC_DBQT, KC_LT,   KC_GT  , KC_QSTM, _______, _______, \
         _______, _______, _______,          _______,                   _______,          _______, _______, _______),
 
-    /* Function Layer 4 F-Key, LED, MISC Control Layer
+    /* Function Layer F-Key, MISC Control Layer
      * ,-----------------------------------------------------------.
-     * | Esc| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10|RSET|
+     * | Esc| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10| Del|
      * |-----------------------------------------------------------|
-     * | Caps | F11| F12| 🔇 | 🔉 | 🔊 |LED+|LED-|LED |LED |       |
+     * | Caps | F11| F12| 🔇 | 🔉 | 🔊 |Home|PgDn|PgUp| End|       |
      * |-----------------------------------------------------------|
-     * |        |MSel| ⏮ | ⏯ | ⏭ | ⏏ |PSCR|SkLk|Paus| Ins | Del|
+     * |        |MSel| ⏮ | ⏯ | ⏭ | ⏏ |PSCR|SkLk|Paus| Ins |RSET|
      * |-----------------------------------------------------------|
      * |     |    |     |           |             | Stop| App|     |
      * `-----------------------------------------------------------'
      */
-    [4] = KEYMAP_LAYOUT_1(
-        KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , RESET  , \
-        KC_CAPS, KC_F11 , KC_F12 , KC_MUTE, KC_VOLD, KC_VOLU, BL_INC , BL_DEC , BL_TOGG, BL_STEP,          TO_1   , \
-        _______,          KC_EJCT, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSEL, KC_PSCR, KC_SLCK, KC_PAUS, KC_INS,  KC_DEL , \
-        _______, _______, _______,          _______,                   _______,          KC_STOP, KC_APP , _______),
-
-
-    /* Layer 5 empty
-    [5] = KEYMAP_LAYOUT_1(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, \
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        _______, _______, _______,          _______,                   _______,          _______, _______, _______),
-    */
+    [9] = KEYMAP_LAYOUT_1(
+        KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_DEL , \
+        KC_CAPS, KC_F11 , KC_F12 , KC_MUTE, KC_VOLD, KC_VOLU, KC_HOME, KC_PGDN, KC_PGUP, KC_END ,          TO_3   , \
+        _______,          KC_EJCT, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSEL, KC_PSCR, KC_SLCK, KC_PAUS, KC_INS,  RESET  , \
+        _______, _______, _______,          TO_1   ,                   TO_2   ,          KC_STOP, KC_APP , _______),
 };
 
 /*
@@ -153,21 +153,26 @@ const uint16_t fn_actions[FN_ACTIONS_COUNT] __attribute__ ((section (".keymap.fn
 #else
 const uint16_t fn_actions[] PROGMEM = {
 #endif
-    [0] = ACTION_LAYER_TOGGLE(1),
-    [1] = ACTION_LAYER_MOMENTARY(2),
-    [2] = ACTION_LAYER_MOMENTARY(3),
-    [3] = ACTION_LAYER_MOMENTARY(4),
-    //[4] = ACTION_LAYER_TAP_KEY(2, KC_SPC),
-    //[5] = ACTION_LAYER_TAP_KEY(3, KC_ENT),
+    // bootloader
+    [0] = ACTION_FUNCTION(BOOTLOADER),         // RESET
+
+    [1] = ACTION_LAYER_TOGGLE(1),
+    [2] = ACTION_LAYER_TOGGLE(2),
+    [3] = ACTION_LAYER_TOGGLE(3),
+    [4] = ACTION_LAYER_TOGGLE(4),
+
+    [5] = ACTION_LAYER_TAP_KEY(7, KC_SPC),
+    [6] = ACTION_LAYER_TAP_KEY(8, KC_ENT),
+
+    [7] = ACTION_LAYER_MOMENTARY(7),
+    [8] = ACTION_LAYER_MOMENTARY(8),
+    [9] = ACTION_LAYER_MOMENTARY(9),
 
     // backlight
-    [6] = ACTION_BACKLIGHT_DECREASE(),
-    [7] = ACTION_BACKLIGHT_INCREASE(),
-    [8] = ACTION_BACKLIGHT_TOGGLE(),
-    [9] = ACTION_BACKLIGHT_STEP(),
-
-    // bootloader
-    [10] = ACTION_FUNCTION(BOOTLOADER),        // RESET
+    //[6] = ACTION_BACKLIGHT_DECREASE(),
+    //[7] = ACTION_BACKLIGHT_INCREASE(),
+    //[8] = ACTION_BACKLIGHT_TOGGLE(),
+    //[9] = ACTION_BACKLIGHT_STEP(),
 
     [11] = ACTION_MODS_KEY(MOD_LSFT, KC_SCLN), // KC_COLN :
     [12] = ACTION_MODS_KEY(MOD_LSFT, KC_QUOT), // KC_DBQT "
